5f3c7d479fd1bafe0bf6b7762b6983332c8a4eee
jonathan dreyfuss <jdreyf@gmail.com>
2024-01-25 17:37:24 -0500

    update test output

--- tests/testthat/test-rmd_links.R
+++ tests/testthat/test-rmd_links.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("links work", {
   rl <- rmd_links(filenames = "heat.pdf")
   expect_equal(rl, "`r make_file_links(wd, 'heat.pdf')`")
 })
--- tests/testthat/test-setup_chunk.R
+++ tests/testthat/test-setup_chunk.R
@@ -1,4 +1,4 @@
 test_that("multiplication works", {
   sc <- setup_chunk("patti")
-  expect_equal(sc[length(sc) - 1], "wd <- J:/cores/bioinformatics/patti")
+  expect_equal(sc[length(sc) - 1], "wd <- \"J:/cores/bioinformatics/patti\"")
 })
